c3acd26b89be74eac6e5102380640cfd84cce55f
Foster McLane <fkmclane@gmail.com>
2013-10-20 20:09:10 -0400

    Filled out Node and Solver; Added javadoc

--- src/Node.java
+++ src/Node.java
@@ -1,32 +1,128 @@
+/**
+ * A single node in the tree of paths
+ */
 public class Node {
 	public Node parent;
 	private double parent_cost;
-	private int index;
-	private int[] constraints;
 
-	public Node left;
-	public Node right;
+	private double[][] locations;
+	private int[] active_set;
 
-	public Node(Node parent, double parent_cost, int index, int[] constraints) {
+	public int index;
+
+	/**
+	 * Constructs a new node
+	 *
+	 * @param parent This node's parent
+	 * @param parent_cost The cost between these nodes
+	 * @param locations The 2D array of distance between locations
+	 * @param active_set The set of all points (including this node) that are being calculated
+	 * @param index The location index of this node
+	 */
+	public Node(Node parent, double parent_cost, double[][] locations, int[] active_set, int index) {
 		this.parent = parent;
 		this.parent_cost = parent_cost;
+		this.locations = locations;
+		this.active_set = active_set;
 		this.index = index;
-		this.constraints = constraints;
-		left = null;
-		right = null;
 	}
 
+	/**
+	 * Check if this node is terminal
+	 *
+	 * @return Whether or not the node is terminal
+	 */
 	public boolean isTerminal() {
-		return left == null && right == null;
+		return active_set.length == 1;
 	}
 
+	/**
+	 * Generate and return this node's children
+	 *
+	 * @precondition Node is not terminal
+	 * @return Array of children
+	 */
+	public Node[] generateChildren() {
+		Node[] children = new Node[active_set.length - 1];
+
+		int[] new_active_set = new int[active_set.length - 1];
+		int i = 0;
+		for(int location : active_set) {
+			if(location == index)
+				continue;
+
+			new_active_set[i] = location;
+			i++;
+		}
+
+		for(int j = 0; j < children.length; j++)
+			children[j] = new Node(this, locations[index][new_active_set[j]], locations, new_active_set, new_active_set[j]);
+
+		return children;
+	}
+
+	/**
+	 * Get the path array up to this point
+	 *
+	 * @return The path
+	 */
+	public int[] getPath() {
+		int depth = locations.length - active_set.length + 1;
+		int[] path = new int[depth];
+		getPathIndex(path, depth - 1);
+		return path;
+	}
+
+	/**
+	 * Recursive method to fill in a path array from this point
+	 *
+	 * @param path The path array
+	 * @param i The index of this node
+	 */
+	public void getPathIndex(int[] path, int i) {
+		path[i] = index;
+		if(parent != null)
+			parent.getPathIndex(path, i - 1);
+	}
+
+	/**
+	 * Get the lower bound cost of this node
+	 *
+	 * @return Lower bound cost
+	 */
 	public double getLowerBound() {
+		double value = 0;
+
+		for(int location : active_set) {
+			double low1 = Double.MAX_VALUE;
+			double low2 = Double.MAX_VALUE;
+
+			for(int other: active_set) {
+				if(other == location)
+					continue;
+
+				double cost = locations[location][other];
+				if(cost < low1)
+					low1 = cost;
+				else if(cost < low2)
+					low2 = cost;
+			}
+
+			value += low1 + low2;
+		}
+
+		return getPathCost() + value / 2;
 	}
 
-	public double getCost() {
+	/**
+	 * Get the cost of the path up to this point
+	 *
+	 * @return Cost of path
+	 */
+	public double getPathCost() {
 		if(parent == null)
 			return parent_cost;
 
-		return parent_cost + parent.getCost();
+		return parent_cost + parent.getPathCost();
 	}
 }
--- src/Solver.java
+++ src/Solver.java
@@ -1,12 +1,84 @@
+import java.util.HashSet;
+
+/**
+ * Solves the travelling salesman problem using Branch and Bound by utilizing Node's
+ */
 public class Solver {
+	double best_cost;
+	int[] best_path;
+
+	/**
+	 * Calculates the shortest (non-repeating) path between a series of nodes
+	 *
+	 * @param locations The 2D array of distance between locations
+	 * @return An integer array with the indices of the locations
+	 */
 	public int[] calculate(double[][] locations) {
-		int[] path = new int[locations.length];
+		//HashSet<Integer> location_set = new HashSet<Integer>(locations.length);
+		//for(int location : locations[0])
+		//	location_set.add(location)
 
-		double best_cost = Double.MAX_VALUE;
+		best_cost = Double.MAX_VALUE; //findGreedyCost(0, location_set, locations);
 
-		for(double[] location : locations) {
+		int[] active_set = new int[locations.length];
+		for(int i = 0; i < active_set.length; i++)
+			active_set[i] = i;
+
+		Node root = new Node(null, 0, locations, active_set, 0);
+		traverse(root);
+
+		return best_path;
+	}
+
+	/**
+	 * Find the greedy cost for a set of locations
+	 *
+	 * @param i The current location
+	 * @param location_set Set of all remaining locations
+	 * @param locations The 2D array containing point distances
+	 * @return The greedy cost
+	 */
+	private double findGreedyCost(int i, HashSet<Integer> location_set, double[][] locations) {
+		if(location_set.isEmpty())
+			return 0;
+
+		location_set.remove(i);
+
+		double lowest = Double.MAX_VALUE;
+		int closest = 0;
+		for(int location : location_set) {
+			double cost = locations[i][location];
+			if(cost < lowest) {
+				lowest = cost;
+				closest = location;
+			}
 		}
 
-		return path;
+		return lowest + findGreedyCost(closest, location_set, locations);
+	}
+
+	/**
+	 * Recursive method to go through the tree finding and pruning paths
+	 *
+	 * @param parent The root/parent node
+	 */
+	private void traverse(Node parent) {
+		Node[] children = parent.generateChildren();
+
+		for(Node child : children) {
+			if(child.getLowerBound() > best_cost)
+				continue;
+
+			if(child.isTerminal()) {
+				double cost = child.getPathCost();
+				if(cost < best_cost) {
+					best_cost = cost;
+					best_path = child.getPath();
+				}
+			}
+			else {
+				traverse(child);
+			}
+		}
 	}
 }
